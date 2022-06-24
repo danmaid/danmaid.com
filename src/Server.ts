09731d6685d6b395f328d1866561586534e43745
danmaid <yamada@danmaid.com>
2022-06-25 01:28:44 +0900

    not found to index.html

--- src/Server.ts
+++ src/Server.ts
@@ -4,6 +4,8 @@ import { Socket } from 'net'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import path from 'path'
+import fs from 'fs'
 
 export class Server extends http.Server {
   wss = new WebSocket.Server({ server: this })
@@ -18,8 +20,15 @@ export class Server extends http.Server {
     app.use(express.json())
     app.use(cors())
     app.use(express.static('./packages/web/dist'))
-    app.route('*').put((req, res) => this.onPUT(req, res))
     app.get('*.json', (req, res) => this.onGET(req, res))
+    app.put('*', (req, res) => this.onPUT(req, res))
+    const indexFile = path.resolve('./packages/web/dist/index.html')
+    try {
+      fs.accessSync(indexFile, fs.constants.R_OK)
+      app.get('*', (req, res) => res.sendFile(indexFile))
+    } catch (err) {
+      console.log('index.html not found.', indexFile)
+    }
     this.on('request', app)
     this.on('connection', (socket) => this.clients.push(socket))
     this.app = app
