5f092311cc14b1b084f79c12bc8741dcd67b1d4c
danmaid <yamada@danmaid.com>
2022-10-24 01:27:11 +0900

    rebuild

--- src/http.ts
+++ src/http.ts
@@ -1,34 +1,18 @@
-import { createServer, IncomingMessage, IncomingHttpHeaders, OutgoingHttpHeaders } from 'http'
+import { createServer, IncomingHttpHeaders, OutgoingHttpHeaders, ServerResponse } from 'node:http'
 import { v4 as uuid } from 'uuid'
-import { Socket, AddressInfo } from 'node:net'
-import { core, Event } from './core'
+import { Event } from './core'
 import { Readable } from 'node:stream'
+import { link, mkdir, appendFile, stat } from 'node:fs/promises'
+import { createWriteStream, mkdirSync, createReadStream } from 'node:fs'
+import { join } from 'node:path'
+import { createInterface } from 'node:readline'
+
+const dataDir = 'data'
+const eventDir = join(dataDir, 'events')
+const eventIndex = join(eventDir, 'index.jsonl')
+mkdirSync(eventDir, { recursive: true })
 
 export const server = createServer()
-
-export interface ConnectionEvent extends Event {
-  type: 'connected' | 'disconnected'
-  connection: string
-  remote?: Partial<AddressInfo>
-}
-
-type SocketWithID = Socket & { id?: string }
-server.on('connection', (socket: SocketWithID) => {
-  const id = uuid()
-  socket.id = id
-  socket.once('close', () => {
-    core.emit<ConnectionEvent>({ type: 'disconnected', connection: id })
-  })
-  core.emit<ConnectionEvent>({
-    type: 'connected',
-    connection: id,
-    remote: {
-      address: socket.remoteAddress,
-      family: socket.remoteFamily,
-      port: socket.remotePort,
-    },
-  })
-})
 
 export interface ResponseEvent extends Event {
   type: 'response'
@@ -51,45 +35,71 @@ export interface RequestEvent extends Event {
   path?: string
 }
 
-function hasContent(headers: IncomingHttpHeaders): boolean {
-  if (headers['content-length']) {
-    const length = parseInt(headers['content-length'])
-    if (length > 0) return true
-  }
-  return false
-}
+server.on('event', (ev) => appendFile(eventIndex, JSON.stringify(ev) + '\n'))
 
-type IncomingMessageWithID = IncomingMessage & { id?: string; socket: SocketWithID }
-server.on('request', async (req: IncomingMessageWithID, res) => {
-  const id = uuid()
-  req.id = id
-  const content = hasContent(req.headers) ? req : undefined
-  const url = req.url ? new URL(req.url, `http://${req.headers.host}`) : undefined
+server.on('request', async (req, res) => {
+  try {
+    const { socket, headers, method, url, httpVersion } = req
+    const id = uuid()
+    const client = { address: socket.remoteAddress, family: socket.remoteFamily, port: socket.remotePort }
+    const request = { client, http: httpVersion, method, url, ...headers }
+    server.emit('event', { id: uuid(), type: 'request', request: id, ...request })
 
-  const wait = core.wait<ResponseEvent>((ev) => ev.type === 'response' && ev.request === id)
-  core.emit<RequestEvent>({
-    type: 'request',
-    request: id,
-    http: req.httpVersion,
-    method: req.method,
-    url: req.url,
-    headers: req.headers,
-    connection: req.socket.id,
-    content,
-    path: url?.pathname,
-  })
-  const response = await wait
-  const { status, statusText, headers } = response
-  res.writeHead(status, statusText, headers)
-  if (response.content) {
-    if (response.content instanceof Readable) {
-      const content = response.content
-      content.pipe(res)
-      await new Promise((r) => content.on('end', r))
-    } else res.write(response.content)
+    if (url) {
+      const { pathname } = new URL(url, `http://${req.headers.host}`)
+      const file = join(dataDir, pathname)
+
+      // PUT
+      const length = headers['content-length']
+      if (method === 'PUT' && length && parseInt(length) > 0) {
+        const content = join(eventDir, id)
+        await new Promise((r) => {
+          const w = createWriteStream(content)
+          w.on('finish', r)
+          req.pipe(w)
+        })
+        await mkdir(file, { recursive: true })
+        await link(content, file)
+        return res.writeHead(200).end()
+      }
+
+      // GET
+      if (method === 'GET') {
+        const meta = await new Promise<Record<string, unknown>>((resolve, reject) => {
+          const rl = createInterface(createReadStream(eventIndex))
+          rl.on('line', (line) => resolve())
+        })
+        const stats = await stat(file)
+      }
+
+      // DELETE
+      // PATCH
+      // POST
+
+      // SSE
+      if (method === 'GET' && headers.accept === 'text/event-stream') {
+        res.writeHead(200, { 'Content-Type': 'text/event-stream' })
+        const { statusCode: status, statusMessage: message } = res
+        const headers = res.getHeaders()
+        server.emit('event', { id: uuid(), type: 'chunked', request: id, status, message, ...headers })
+        server.on('event', (ev: { type?: string; id?: string }) => {
+          if (ev.type) res.write(`event: ${ev.type}\n`)
+          if (ev.id) res.write(`id: ${ev.id}\n`)
+          res.write(`data: ${JSON.stringify(ev)}\n\n`)
+        })
+      }
+    }
+
+    res.on('close', () => {
+      const { statusCode: status, statusMessage: message } = res
+      const headers = res.getHeaders()
+      server.emit('event', { id: uuid(), type: 'response', request: id, status, message, ...headers })
+    })
+    res.writeHead(501).end()
+  } catch (err) {
+    console.error(err)
+    res.writeHead(500).end()
   }
-  res.on('finish', () => core.emit<RequestEvent>({ type: 'responded', request: id }))
-  res.end()
 })
 
 server.listen(8520, () => {
