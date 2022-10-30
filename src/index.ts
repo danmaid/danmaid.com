28ae2dd4a3757efa08673b0ce7f92b431e61114f
danmaid <yamada@danmaid.com>
2022-10-30 14:36:22 +0900

    post events

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import http from 'node:http'
 import express from 'express'
 import { todos } from './todos'
+import { sensors } from './sensors'
 
 export class Server extends http.Server {
   constructor(public app = express()) {
     super(app)
     app.use(todos)
+    app.use(sensors)
   }
 }
---
+++ src/sensors.ts
@@ -0,0 +1,69 @@
+import { Router, json } from 'express'
+import { v4 as uuid } from 'uuid'
+import { readdir, readFile, writeFile } from 'node:fs/promises'
+import { mkdirSync } from 'node:fs'
+import { join } from 'node:path'
+
+export const sensors = Router()
+
+const dir = './data/sensors'
+mkdirSync(dir, { recursive: true })
+const eventDir = './data/events'
+mkdirSync(eventDir, { recursive: true })
+
+sensors.use(json())
+
+const items = sensors.route('/sensors')
+
+items.get(async ({ query }, res, next) => {
+  const files = await readdir(dir)
+  const items = files.map(async (file) => {
+    const text = await readFile(join(dir, file), { encoding: 'utf-8' })
+    const data = JSON.parse(text)
+    return { ...data, id: file }
+  })
+  const data = await Promise.all(items)
+  if (Object.keys(query).length > 0) {
+    const filtered = data.filter((item) => {
+      return Object.entries(query).every(([k, v]) => {
+        return typeof v === 'string' && v.startsWith('!') ? item[k] !== v.slice(1) : item[k] === v
+      })
+    })
+    res.json(filtered)
+  } else res.json(data)
+})
+
+const item = sensors.route('/sensors/:id')
+
+item.get(async ({ params: { id } }, res, next) => {
+  try {
+    const text = await readFile(join(dir, id), { encoding: 'utf-8' })
+    const data = JSON.parse(text)
+    res.json({ ...data, id })
+  } catch {
+    res.sendStatus(404)
+  }
+})
+
+const events = sensors.route('/sensors/:id/events')
+
+events.post(async ({ params: { id }, body }, res, next) => {
+  try {
+    const text = await readFile(join(dir, id), { encoding: 'utf-8' })
+    const sensor = JSON.parse(text)
+
+    const eventId = uuid()
+    const event = { ...body, date: new Date(), sensor: id }
+    await writeFile(join(eventDir, eventId), JSON.stringify(event))
+
+    await writeFile(join(dir, id), JSON.stringify({ ...sensor, ...body, event: eventId }))
+    res.sendStatus(201)
+  } catch {
+    const eventId = uuid()
+    const event = { ...body, date: new Date(), sensor: id }
+    await writeFile(join(eventDir, eventId), JSON.stringify(event))
+
+    await writeFile(join(dir, id), JSON.stringify({ ...event, event: eventId, ...body }))
+    res.sendStatus(201)
+  }
+})
