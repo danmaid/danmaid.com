72abb94ce84e61c88b676a046952462885b53043
danmaid <yamada@danmaid.com>
2022-11-01 09:58:02 +0900

    performance tuned

--- src/events.ts
+++ src/events.ts
@@ -2,10 +2,13 @@ import { mkdirSync } from 'node:fs'
 import { EventEmitter } from 'node:events'
 import { v4 as uuid } from 'uuid'
 import { join } from 'node:path'
-import { readdir, readFile, writeFile } from 'node:fs/promises'
+import { appendFile } from 'node:fs/promises'
+import { createReadStream } from 'node:fs'
+import { createInterface } from 'node:readline'
 
 export class EventStore<T = any> extends EventEmitter {
   dir = './data/events'
+  file = join(this.dir, 'index.jsonl')
 
   constructor() {
     super()
@@ -15,19 +18,22 @@ export class EventStore<T = any> extends EventEmitter {
   async add(event: T): Promise<string> {
     const id = uuid()
     const e = { id, date: new Date(), event }
-    await writeFile(join(this.dir, id), JSON.stringify(e))
+    await appendFile(this.file, JSON.stringify(e) + '\n')
     this.emit('added', e)
     return id
   }
 
   async filter(filter: (v: T & { id: string }) => boolean): Promise<(T & { id: string })[]> {
-    const files = await readdir(this.dir)
-    const items = files.map(async (file): Promise<T & { id: string; date: string }> => {
-      const text = await readFile(join(this.dir, file), { encoding: 'utf-8' })
-      return JSON.parse(text)
+    return await new Promise((resolve, reject) => {
+      const events: (T & { id: string })[] = []
+      const reader = createInterface(createReadStream(this.file))
+      reader.on('line', (line) => {
+        const event = JSON.parse(line)
+        if (filter(event)) events.push(event)
+      })
+      reader.on('error', reject)
+      reader.on('close', () => resolve(events))
     })
-    const data = await Promise.all(items)
-    return data.filter(filter).sort((a, b) => (a.date > b.date ? 1 : a.date < b.date ? -1 : 0))
   }
 }
 
