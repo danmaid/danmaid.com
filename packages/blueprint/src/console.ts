b53c586aa82d3ad4f5b23a2237ba931506b6cc86
danmaid <yamada@danmaid.com>
2022-11-03 10:55:46 +0900

    implement

---
+++ packages/blueprint/src/console.ts
@@ -0,0 +1,43 @@
+class DmConsole extends HTMLElement {
+  out: HTMLElement
+  src?: EventSource
+
+  constructor() {
+    super()
+    const root = this.attachShadow({ mode: 'open' })
+    root.innerHTML = `
+      <div style="display: flex; flex-direction: column; height: 100%">
+        <div id="out" style="flex: 1 0; background-color: #00f3; display: flex; flex-direction: column-reverse; overflow: auto"></div>
+        <div style="flex: 0 0; background-color: #0f03"></div>
+      </div>
+    `
+    const out = root.getElementById('out')
+    if (!out) throw Error('')
+    this.out = out
+  }
+
+  connectedCallback() {
+    if (!this.isConnected) return
+    const url = this.getAttribute('src')
+    if (url) {
+      this.src = new EventSource(url)
+      this.src.onmessage = (ev) => this.onmessage(ev)
+    }
+  }
+
+  disconnectedCallback() {
+    this.src?.close()
+  }
+
+  onmessage(ev: MessageEvent) {
+    const div = document.createElement('div')
+    div.textContent = ev.data
+    this.out.append(div)
+  }
+
+  clear() {
+    this.out.innerHTML = ''
+  }
+}
+
+customElements.define('dm-console', DmConsole)
