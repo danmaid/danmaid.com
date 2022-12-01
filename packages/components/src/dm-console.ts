8c100ab68e1678551ccd9f35b0654291c1b24e75
danmaid <yamada@danmaid.com>
2022-12-01 20:15:06 +0900

    console 移植

--- packages/components/src/dm-console.ts
+++ packages/components/src/dm-console.ts
@@ -2,14 +2,33 @@ import { DmDataView } from './dm-data-view.js'
 
 export class DmConsole<T extends Record<string, unknown> = Record<string, unknown>> extends DmDataView<T> {
   views = []
+  wrapper = document.createElement('div')
+  output = document.createElement('div')
+  input = document.createElement('div')
+
+  constructor() {
+    super()
+    this.wrapper.style.display = 'flex'
+    this.wrapper.style.flexDirection = 'column'
+    this.wrapper.style.height = '100%'
+    this.output.style.flex = '1 0'
+    this.output.style.backgroundColor = '#00f3'
+    this.output.style.overflow = 'auto'
+    this.output.style.whiteSpace = 'nowrap'
+    this.input.style.flex = '0 0'
+    this.input.style.backgroundColor = '#0f03'
+    this.input.innerHTML = '<slot name="input"></slot>'
+    this.wrapper.append(this.output, this.input)
+    this.root.append(this.wrapper)
+  }
+
+  clear() {}
 
   renderItem(item: T) {
-    const view = this.views.find((v) => v)
-    if (!view) return
-    const r = document.createElement(view)
     const div = document.createElement('div')
     div.textContent = JSON.stringify(item)
-    this.root.append(div)
+    this.output.append(div)
+    div.scrollIntoView(false)
   }
 }
 
