2c1468e894a2fe45556cfa80f3de17c5ca1af17b
danmaid <yamada@danmaid.com>
2022-12-30 16:51:37 +0900

    EventProxy

--- src/cli.ts
+++ src/cli.ts
@@ -2,15 +2,30 @@ import { Server } from '.'
 import { program } from 'commander'
 import morgan from 'morgan'
 import express from 'express'
+import EventSource from 'eventsource'
+import { events } from './events'
 
 program
   .command('start')
   .description('start server')
   .option('-p, --port <number>', 'listen port', (v) => parseInt(v), 8520)
   .option('--web')
-  .action(({ port, web }) => {
+  .option('--event-proxy')
+  .action(({ port, web, eventProxy }) => {
     const app = express()
     app.use(morgan('combined'))
+    if (eventProxy) {
+      const proxy = new EventSource('https://labo.danmaid.com')
+      proxy.onmessage = (ev) => {
+        try {
+          const event = JSON.parse(ev.data)
+          events.add(event.event)
+        } catch (e) {
+          console.error('proxy failed.', e)
+        }
+      }
+      proxy.onopen = () => console.log('event proxy started.')
+    }
     if (web) {
       const serve = express.static('packages/web/public')
       app.use((req, res, next) => {
---
+++ tests/eventProxy.spec.ts
@@ -0,0 +1,1 @@
+it.todo('--event-proxy が指定されて起動した場合、 SSE のリクエストを danmaid.com へ中継すること')
