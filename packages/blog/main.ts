73d9bb36b97a46032f80b15d4a1bbcad28dff9f6
danmaid <yamada@danmaid.com>
2023-04-05 14:37:16 +0900

    build

--- packages/blog/main.ts
+++ packages/blog/main.ts
@@ -1,16 +1,38 @@
 import { JSDOM } from 'jsdom'
-import { readFile, writeFile } from 'fs/promises'
+import { readFile, writeFile, readdir, copyFile, mkdir, rm } from 'fs/promises'
 import { marked } from 'marked'
 import { format } from 'prettier'
+import { join, basename, dirname } from 'node:path'
 
-const dom = await JSDOM.fromFile('template.html')
-const main = dom.window.document.querySelector('#main')
+const src = './posts'
+const dist = './dist'
 
-const content = await readFile('src/test.md', { encoding: 'utf-8' })
+await rm(dist, { recursive: true })
+await mkdir(dist)
+await copyRecursive(src, dist)
 
-if (!main) throw Error('main not found.')
-main.innerHTML = await marked(content, { async: true })
+async function copyRecursive(src: string, dist: string): Promise<void> {
+  for (const file of await readdir(src, { withFileTypes: true })) {
+    if (file.isFile()) {
+      await copyFile(join(src, file.name), join(dist, file.name))
+      if (file.name.endsWith('.md')) await mdToHtml(join(src, file.name), join(dist, file.name))
+    }
+    if (file.isDirectory()) {
+      await mkdir(join(dist, file.name))
+      await copyRecursive(join(src, file.name), join(dist, file.name))
+    }
+  }
+}
 
-const html = format(dom.serialize(), { parser: 'html' })
-console.log(html)
-await writeFile('dist/test.html', html, { encoding: 'utf-8' })
+async function mdToHtml(src: string, dist: string): Promise<void> {
+  const dom = await JSDOM.fromFile('template.html')
+  const main = dom.window.document.querySelector('#main')
+
+  const content = await readFile(src, { encoding: 'utf-8' })
+
+  if (!main) throw Error('invalid template. #main not found.')
+  main.innerHTML = await marked(content, { async: true })
+
+  const html = format(dom.serialize(), { parser: 'html' })
+  await writeFile(join(dirname(dist), basename(dist, '.md') + '.html'), html, { encoding: 'utf-8' })
+}
