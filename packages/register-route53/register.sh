6db87e100ef14947366103a46f7340fa23696150
danmaid <yamada@danmaid.com>
2023-12-08 10:11:34 +0900

    dns01

--- packages/register-route53/register.sh
+++ packages/register-route53/register.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IP=$(curl -s ipinfo.io/ip)
+IP=$(curl -s -4 ipconfig.io/ip)
 JSON=$(cat <<-END
   {
     "Changes": [
@@ -9,6 +9,30 @@ JSON=$(cat <<-END
         "ResourceRecordSet": {
           "Name": "${DNS_DOMAIN_NAME}",
           "Type": "A",
+          "TTL": 60,
+          "ResourceRecords": [
+            {
+              "Value": "${IP}"
+            }
+          ]
+        }
+      }
+    ]
+  }
+END
+)
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch "$JSON"
+
+IP=$(curl -s -6 ipconfig.io/ip)
+JSON=$(cat <<-END
+  {
+    "Changes": [
+      {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+          "Name": "${DNS_DOMAIN_NAME}",
+          "Type": "AAAA",
           "TTL": 60,
           "ResourceRecords": [
             {
